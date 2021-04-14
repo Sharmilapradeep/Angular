eae43e8349401d165fa85e0ac409f71b0aec6070
sharmila <sharmila28shiva@gmail.com>
2021-04-15 00:11:27 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
-import {MatTableModule} from '@angular/material/table';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatIconModule} from '@angular/material/icon';
 import { TableComponent } from './table/table.component';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -19,21 +11,12 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     TableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTableModule,
-    MatAutocompleteModule,
-    MatInputModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatButtonModule,
-    MatMenuModule,
-    MatIconModule,
     HttpClientModule
   ],
   providers: [],
